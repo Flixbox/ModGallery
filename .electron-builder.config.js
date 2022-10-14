8424c77039ad3e5a9d6591f14e5e1ddd05a91fb7
Felix Tietjen <felix@tietjen.it>
2022-10-14 02:23:07 +0200

    remove build from electron builder (hopefully)

--- .electron-builder.config.js
+++ .electron-builder.config.js
@@ -10,6 +10,7 @@ if (process.env.VITE_APP_VERSION === undefined) {
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
+  npmRebuild: false,
   directories: {
     output: 'dist',
     buildResources: 'buildResources',
