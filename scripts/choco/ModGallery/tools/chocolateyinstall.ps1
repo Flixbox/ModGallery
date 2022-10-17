70ca9bbc10e260d0d3c0c3bc8912739f7746313f
Felix Tietjen <felix@tietjen.it>
2022-10-17 13:57:57 +0200

    choco cleanup

--- scripts/choco/ModGallery/tools/chocolateyinstall.ps1
+++ scripts/choco/ModGallery/tools/chocolateyinstall.ps1
@@ -1,4 +1,3 @@
-
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/Flixbox/ModGallery/releases/download/v22.289.1300/modgallery-Setup-22.289.1300.exe'
@@ -8,32 +7,10 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-
   softwareName  = 'ModGallery*'
-
   checksum      = '8259871B806EA105C2AAE91617F0C546'
-
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
